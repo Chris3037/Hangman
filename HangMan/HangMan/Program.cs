37e6aea094762a651fffd1275a4a53dd7f708480
SeedPaths-User <lessdismissive@email.com>
2014-05-01 16:59:53 -0600

    V 3.0

--- HangMan/HangMan/Program.cs
+++ HangMan/HangMan/Program.cs
@@ -10,11 +10,17 @@ namespace HangMan
     class Program
     {
 
-        //Puulbic Variable
+        //Pulbic Variable
         static string userName = "";
         static string input = "";
-        static List<string> lettersGuessed = new List<string>();
+        static List<string> lettersGuessed = new List<string>() { };
         static int guessesLeft = 10;
+        static bool gotOne = false;
+        static bool wordGuessed = false;
+        static string[] wordArray = { "fuchsia", "blue", "white", "orange", "brown", "pink", "purple", "red", "black", "green", "yellow" };
+        static System.Random randomTemp = new Random();
+        static int random = randomTemp.Next(0, wordArray.Length);
+        static string word = wordArray[random];
 
         static void Main(string[] args)
         {
@@ -28,7 +34,7 @@ namespace HangMan
         //Welcome Function
         static void Welcome()
         {
-            Print("Welcome to the game "+userName);
+            Print("Welcome to the game " + userName);
             Thread.Sleep(500);
             Print("You will be guessing letters to find a randomly generated word");
             Thread.Sleep(500);
@@ -47,11 +53,119 @@ namespace HangMan
         static void Stats()
         {
             Console.Clear();
-            Print("Letters Guessed: " + lettersGuessed);
-            Print("Guesses Left: " + guessesLeft);
+            Console.Write("Letters Guessed: ");
+            foreach (var item in lettersGuessed)
+            {
+                Console.Write(item.ToUpper() + " ");
+            }
             Console.WriteLine();
+            Print("Guesses Left: " + guessesLeft);
+            Console.WriteLine("Word: " + Mask(word, lettersGuessed));
+            Console.ForegroundColor = ConsoleColor.Green;
             Print("Enter a letter or word now");
-            input = Console.ReadLine().ToString();
+            Console.ResetColor();
+            Console.WriteLine();
+            input = Console.ReadLine().ToString().ToLower();
+            Add();
+        }
+
+        //Add Function
+        static void Add()
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (input == word[i].ToString())
+                {
+                    gotOne = true;
+                }
+                else if (input == word)
+                {
+                    wordGuessed = true;
+                }
+            }
+
+            if (gotOne)
+            {
+                Print("Good job, you guessed a letter!");
+                lettersGuessed.Add(input);
+                gotOne = false;
+                Thread.Sleep(1500);
+                Stats();
+            }
+            else if (wordGuessed)
+            {
+                Winner();
+            }
+            else if (guessesLeft == 0)
+            {
+                Loser();
+            }
+            else
+            {
+                Print("The letter you guessed was not in the word");
+                lettersGuessed.Add(input);
+                guessesLeft--;
+                Thread.Sleep(1500);
+                Stats();
+            }
+        }
+
+        //Mask Function
+        static string Mask(string word, List<string> guessedLetters)
+        {
+            string returnString = "";
+            int i = 0;
+            bool gotOne = false;
+
+            while (i < word.Length)
+            {
+                var letter = word[i].ToString();
+                foreach (var item in guessedLetters)
+                {
+                    if (item == letter)
+                    {
+                        gotOne = true;
+                    }
+                }
+                if (gotOne)
+                {
+                    returnString += letter.ToUpper() + " ";
+                    gotOne = false;
+                }
+                else
+                {
+                    returnString += "_ ";
+                }
+                i++;
+            }
+            return returnString;
+        }
+
+        //Winner Function
+        static void Winner()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Print("Congratulations " + userName + ". You win!");
+            Console.ResetColor();
+            Console.WriteLine();
+            Print("Guesses Left: " + guessesLeft);
+            Print("Word: " + word.ToUpper());
+            End();
+        }
+
+        //Loser Function
+        static void Loser()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Print("Oh, no " + userName + "! You Lose");
+            Console.ResetColor();
+            Print("You took too many guesses to guess the word");
+            Console.WriteLine();
+            Print("Guesses Left: " + guessesLeft);
+            Print("Word: " + word.ToUpper());
+            End();
         }
 
         //Print Function
